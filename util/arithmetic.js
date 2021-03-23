f51d56fdcbd0f3751887321f0c980398a6385f39
Ace_BCB <benjamincb@hotmail.dk>
2021-03-23 12:25:03 +0100

    Table generation is ready, and export/import is set to ES6 syntax

--- util/arithmetic.js
+++ util/arithmetic.js
@@ -1,4 +1,6 @@
-polyDivision([0, 0, 9, 7, 5, 6, 8, 4], [2, 3, 1, 0, 0, 0, 0, 0])
+export {generateTables};
+
+// polyDivision([0, 0, 9, 7, 5, 6, 8, 4], [2, 3, 1, 0, 0, 0, 0, 0])
 /*
  * Input: dividend and divisor both arrays of integers representing polynomials
  * Output: remainder again as an array representing a polynomial
@@ -27,7 +29,17 @@ function polyDivision(dividend, divisor) {
     return polyDivision(sum, divisor);
 }
 
+/*
+ * Input: size of galois field in bits, generator for this in table as a number
+ *
+ * Output:
+ * two tables first toIndex which takes a polinomial form as a number of an element
+ * and gives the index the polynomial form 0 has no defined value index in this table
+ * so toIndex[0] is undefined
+ * second is toPoly which given an element index gives the polynomial form as a number
+ */
 function generateTables(size, generator) {
+    // we do not generate tables less than 2
     if (size < 2) {
         throw new Error("Cannot generate tables with a symbol size less than 2");
     }
@@ -35,5 +47,47 @@ function generateTables(size, generator) {
     let toIndex = [],
         toPoly  = [];
 
-    // we skip index 00 because
+    // we skip index 00 because it creates weird indexing and is not needed
+    // this means we need an extra case for 00 when we multiply and divide
+    toIndex[1] = 0;
+    toPoly[0] = 1;
+
+    // current polynomial which we are calculating
+    // highestBit in our generator 25 would be 16 because 25 = 0b11001
+    let current    = 1,
+        highestBit = findHighestBit(generator);
+
+    for (let i = 1; i < size; i++) {
+        // multiply by alpha
+        current <<= 1;
+
+        // check to see if we are out of the field and corret it if we are
+        if (hasBit(current, highestBit)) {
+            current ^= generator;
+        }
+
+        toIndex[current] = i % (size - 1);
+        toPoly[i] = current;
+    };
+
+    return [toIndex, toPoly];
+}
+
+/*
+ * Input: num which we want to find the highest bit in
+ * Output: value of the highest bit
+ */
+function findHighestBit(num) {
+    let bit = 1;
+    while (num > bit) bit <<= 1;
+    bit >>= 1;
+    return bit;
+}
+
+/*
+ * Input: number that we want to check and the value of the bit position that we want to check
+ * Output: True if it contains contains that bit and false otherwise
+ */
+function hasBit(num, bit) {
+    return (num > (num ^ bit));
 }
