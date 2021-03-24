00973507a65db0601772a0803b10ed03ed8fcedb
Ace_BCB <benjamincb@hotmail.dk>
2021-03-24 11:40:38 +0100

    argument parsing is set up, along with the arguments e, d and h

--- src/main.js
+++ src/main.js
@@ -2,8 +2,43 @@ import fs from 'fs';
 import {generateTables} from "../util/arithmetic.js";
 export {config, toIndex, toPoly};
 
-const   projectRoot = process.cwd(),
-        configFile = fs.readFileSync(projectRoot + "/config.json"),
-        config = JSON.parse(configFile),
-        [toIndex, toPoly] = generateTables();
+const projectRoot = process.cwd();
+const configFile = fs.readFileSync(projectRoot + "/config.json");
+let config = JSON.parse(configFile);
 
+const [toIndex, toPoly] = generateTables();
+
+parseArgs(process.argv);
+
+/*
+ * Input: process.argv is these are the user inputs when starting the program
+ * Output: changes the config object arrocdingly are does nothing if no valid arguments are given
+ */
+function parseArgs(args) {
+    // loop through all the arguments
+    for (let arg of args) {
+        // if the - prefix is not present we assumme it is not our argument
+        if (arg[0] !== '-') continue;
+
+        // remove the prefix
+        arg = arg.substring(1);
+
+        // find the argument in our list of supported arguments or discard invalid arguments
+        switch (arg) {
+            case 'e': config.mode = "encode"; break;
+            case 'd': config.mode = "decode"; break;
+            case 'h': help(); break;
+            default : console.log(arg + " is not a valid argument try -h"); break;
+        };
+    };
+
+    // display help information about each argument
+    function help() {
+        console.log("Some argements like e and d are mutually exclusive speciying both",
+                    "will result in the latter overwriting the first.\n");
+        console.log("If they are not mutually exclusive it will result in the arguments",
+                    "being run twice -h -h for instance\n");
+        console.log("e: sets the program to encoding mode\n");
+        console.log("d: sets the program in decoding mode\n");
+    }
+}
