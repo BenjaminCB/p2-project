f0f5d55cc2c389a9596e921e6877b0aa6dba5b44
Ace_BCB <benjamincb@hotmail.dk>
2021-03-29 12:15:37 +0200

    update file structure to greater reflect the use of functions

--- test/setup_test.js
+++ test/setup_test.js
@@ -1,6 +1,21 @@
 import test from 'ava';
+import {isRS} from "./arithmetic_test.js";
 import {config} from "../src/main.js";
-import {codeGenerator} from "../src/setup.js";
+import {codeGenerator, generateTables} from "../src/setup.js";
+
+test("Field generation", t => {
+    if (!isRS(15, 11, 4, 25)) {
+        t.pass();
+    } else {
+        let [toIndexActual, toPolyActual] = generateTables();
+        toIndexActual.shift();
+
+        let toIndexExpected = [0, 1, 12, 2, 9, 13, 7, 3, 4, 10, 5, 14, 11, 8, 6];
+        let toPolyExpected = [1, 2, 4, 8, 9, 11, 15, 7, 14, 5, 10, 13, 3, 6, 12, 1];
+        t.deepEqual(toIndexExpected, toIndexActual);
+        t.deepEqual(toPolyExpected, toPolyActual);
+    }
+});
 
 test("Code generator", t => {
     if (config.codeSize != 15 ||
