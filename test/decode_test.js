f232ae61bf944a33a14a6f92e069003b92828e3e
Ace_BCB <benjamincb@hotmail.dk>
2021-03-31 11:21:27 +0200

    made more tests for the decoding process

--- test/decode_test.js
+++ test/decode_test.js
@@ -2,24 +2,33 @@ import test from 'ava';
 import * as decode from "../src/decode.js";
 import {isRS} from "./arithmetic_test.js";
 
-test("Berlekamp algorithm", t => {
-    if (isRS(8, 6, 4, 25)) {
-        let expected = [1, 9];
-        let actual   = decode.berlekamp([1, 13]);
-        t.deepEqual(expected, actual);
+test("Syndrome calculation", t => {
+    if (isRS(15, 11, 4, 19)) {
+        let rec = [12, 12, 1, 3, 11, 10, 9, 8, 7, 11, 5, 4, 3, 2, 1];
+        t.deepEqual([15, 3, 4, 12], decode.calcSyndromes(rec));
     } else if (isRS(15, 11, 4, 25)) {
-        let expected = [1, 14, 14];
-        let actual   = decode.berlekamp([15, 3, 4, 12]);
+        let rec = [12, 9, 3, 3, 11, 10, 9, 8, 7, 6, 5, 14, 3, 2, 1],
+            actual = decode.calcSyndromes(rec),
+            expected = [15, 12, 5, 1];
         t.deepEqual(expected, actual);
     } else {
         t.pass();
     }
 });
 
-test("Syndrome calculation", t => {
-    if (isRS(15, 11, 4, 19)) {
-        let rec = [12, 12, 1, 3, 11, 10, 9, 8, 7, 11, 5, 4, 3, 2, 1];
-        t.deepEqual([15, 3, 4, 12], decode.calcSyndromes(rec));
+test("Berlekamp algorithm", t => {
+    if (isRS(8, 6, 4, 25)) {
+        let expected = [1, 9];
+        let actual   = decode.berlekamp([1, 13]);
+        t.deepEqual(expected, actual);
+    } else if (isRS(15, 11, 4, 19)) {
+        let expected = [1, 14, 14];
+        let actual   = decode.berlekamp([15, 3, 4, 12]);
+        t.deepEqual(expected, actual);
+    } else if (isRS(15, 11, 4, 25)) {
+        let expected = [1, 12, 15];
+        let actual   = decode.berlekamp([15, 12, 5, 1]);
+        t.deepEqual(expected, actual);
     } else {
         t.pass();
     }
@@ -29,6 +38,10 @@ test("Chien search", t => {
     if (isRS(15, 11, 4, 19)) {
         let expected = [6, 13];
         let actual = decode.chien([1, 14, 14]);
+        t.deepEqual(expected, actual);
+    } else if (isRS(15, 11, 4, 25)) {
+        let expected = [4, 14];
+        let actual = decode.chien([1, 12, 15]);
         t.deepEqual(expected, actual);
     } else {
         t.pass();
@@ -40,5 +53,11 @@ test("Forney", t => {
         let expected = [13, 2];
         let actual = decode.forney([1, 14, 14], [15, 3, 4, 12], [6, 13]);
         t.deepEqual(expected, actual);
+    } else if (isRS(15, 11, 4, 25)) {
+        let expected = [10, 5];
+        let actual = decode.forney([1, 12, 15], [15, 12, 5, 1], [4, 14]);
+        t.deepEqual(expected, actual);
+    } else {
+        t.pass();
     }
 });
