67b4b16503a1249859ff3966ba80c7e892f36dbb
Ace_BCB <benjamincb@hotmail.dk>
2021-04-16 10:48:40 +0200

    added basic http server

---
+++ src/server2.js
@@ -0,0 +1,36 @@
+import http from "http";
+import websocket from "websocket";
+import fs from "fs";
+import url from "url";
+
+const clientHTML = fs.readFileSync("./src/index.html");
+
+let connections = [];
+
+let server = new http.Server();
+server.listen(3000);
+
+let wsServer = new websocket.server({
+    httpServer: server
+});
+
+server.on("request", (req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    if (pathname === "/") {
+        res.writeHead(200, {"Content-Type": "text/html"}).end(clientHTML);
+    } else {
+        res.writeHead(404).end();
+    }
+});
+
+wsServer.on('request', (req) => {
+    console.log((new Date()) + ' Connection from origin ' + req.origin + '.');
+    let connection = req.accept(null, req.origin);
+    connections.push(connection);
+    console.log((new Date()) + ' Connection accepted.');
+
+    // connection.on('message', msg => {
+        // console.log(JSON.stringify(msg));
+        // connections.forEach(c => c.sendUTF( JSON.stringify(msg) ));
+    // });
+});
