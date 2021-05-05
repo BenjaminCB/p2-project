26cd5cca4575f3fbdab9c0ed3f1add044fc566d0
Ace_BCB <benjamincb@hotmail.dk>
2021-05-05 12:32:17 +0200

    added slash to convert windows fath to posix compliant path

--- website/server.js
+++ website/server.js
@@ -3,6 +3,7 @@ import websocket from "websocket";
 import fs from "fs";
 import url from "url";
 import shelljs from "shelljs";
+import slash from "slash";
 import * as data from "../util/data_processing.js";
 
 const cwd = process.cwd();
@@ -53,7 +54,7 @@ wsServer.on('request', (req) => {
         fs.writeFileSync(config.inputFile, msg.utf8Data);
 
         // let the other programs handle encoding decoding etc...
-        shelljs.exec(cwd + "/run.sh");
+        shelljs.exec(slash(cwd) + "/run.sh");
 
         // read and send the encoding message
         let event = "encode=";
